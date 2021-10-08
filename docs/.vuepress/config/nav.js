39eff88bd30b3a61ff727a75f190002f09055a3c
ceciFang <1298397370@qq.com>
2021-10-08 15:11:47 +0800

    更新

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -2,51 +2,53 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: '前端',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    text: 'web 前端',
+    link: '/web/',
     items: [
-      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
-        text: 'Web 三剑客',
+        text: '前端基础',
         items: [
-          { text: 'HTML', link: '/pages/3dbf66/' },
-          { text: 'CSS', link: '/pages/9fb2d5/' },
-          { text: 'JavaScript', link: '/pages/58f512/' },
+          { text: 'web三剑客', link: '/pages/3dbf66/' },
+          { text: '新技术', link: '/pages/9fb2d5/' },
+          { text: '开发环境', link: '/pages/58f512/' },
         
         ],
       },
       {
-        text: '其他',
+        text: '前端框架/库',
         items: [
-          // { text: 'ES6', link: '/pages/7220f2/' },
-          { text: '前端框架', link: '/pages/602330/' },
-          { text: '打包工具', link: '/pages/a6ef4b/' },
-          // { text: '浏览器', link: '/pages/4e9cb9/' },
-          // { text: '前端性能优化', link: '/pages/4e9cb9/' },
+          { text: 'Vue', link: '/pages/602330/' },
+          // { text: 'React', link: '/pages/a6ef4b/' },
+          { text: 'Webpack', link: '/pages/4e9cb9/' },
+          { text: 'Echarts', link: '/pages/4e9cb9/' },
         ],
       },
     ],
   },
   {
-    text: '后端',
-    link: '/backend/',
+    text: '大前端',
+    link: '/superWeb/',
     items: [
-      { text: 'Koa', link: '/pages/dc86e3/' },
-      // { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
+      {
+        text: '大前端',
+        items: [
+          { text: 'Koa', link: '/pages/dc86e3/' },
+      { text: 'nodejs', link: '/pages/117708e0af7f0bd9' },
+        
+        ],
+      },
     ],
   },
   {
-    text: '技术',
-    link: '/technology/',
+    text: '学习',
+    link: '/learn/',
     items: [
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      { text: 'GitHub技巧', link: '/pages/538ee7/' },
-      // { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+      { text: '答疑解惑文档', link: '/pages/9e2549/' },
     ],
   },
   {
-    text: '更多',
+    text: '叨叨念念',
     link: '/more/',
     items: [
       { text: '学习', link: '/pages/f2a556/' },
@@ -58,13 +60,8 @@ module.exports = [
   },
   { text: '关于', link: '/about/' },
   {
-    text: '收藏',
+    text: '宝藏',
     link: '/pages/beb6c0bd8a66cea6/',
-    // items: [
-    //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-    //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-    //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-    // ],
   },
   {
     text: '索引',
@@ -75,4 +72,80 @@ module.exports = [
       { text: '归档', link: '/archives/' },
     ],
   },
+
+
+
+  // { text: '首页', link: '/' },
+  // {
+  //   text: '前端',
+  //   link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+  //   items: [
+  //     // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+  //     {
+  //       text: 'Web 三剑客',
+  //       items: [
+  //         { text: 'HTML', link: '/pages/3dbf66/' },
+  //         { text: 'CSS', link: '/pages/9fb2d5/' },
+  //         { text: 'JavaScript', link: '/pages/58f512/' },
+        
+  //       ],
+  //     },
+  //     {
+  //       text: '',
+  //       items: [
+  //         // { text: 'ES6', link: '/pages/7220f2/' },
+  //         { text: '前端框架/库', link: '/pages/602330/' },
+  //         { text: '打包工具', link: '/pages/a6ef4b/' },
+  //         { text: 'Web服务', link: '/pages/4e9cb9/' },
+  //         // { text: '前端性能优化', link: '/pages/4e9cb9/' },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // {
+  //   text: '大前端',
+  //   link: '/backend/',
+  //   items: [
+  //     { text: 'Koa', link: '/pages/dc86e3/' },
+  //     { text: 'nodejs', link: '/pages/117708e0af7f0bd9' },
+  //   ],
+  // },
+  // {
+  //   text: '技术',
+  //   link: '/technology/',
+  //   items: [
+  //     { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+  //     { text: '答疑解惑文档', link: '/pages/9e2549/' },
+  //   ],
+  // },
+  // {
+  //   text: '更多',
+  //   link: '/more/',
+  //   items: [
+  //     { text: '学习', link: '/pages/f2a556/' },
+  //     { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+  //     // { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+  //     // { text: '实用技巧', link: '/pages/baaa02/' },
+  //     // { text: '友情链接', link: '/friends/' },
+  //   ],
+  // },
+  // { text: '关于', link: '/about/' },
+  // {
+  //   text: '收藏',
+  //   link: '/pages/beb6c0bd8a66cea6/',
+  //   // items: [
+  //   //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
+  //   //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+  //   //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
+  //   // ],
+  // },
+  // {
+  //   text: '索引',
+  //   link: '/archives/',
+  //   items: [
+  //     { text: '分类', link: '/categories/' },
+  //     { text: '标签', link: '/tags/' },
+  //     { text: '归档', link: '/archives/' },
+  //   ],
+  // },
 ]
