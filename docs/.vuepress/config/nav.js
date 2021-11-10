8bacc3176fa5668c592681740ea46b189c59b18d
ceciFang <1298397370@qq.com>
2021-11-10 16:50:15 +0800

    add like

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -51,8 +51,8 @@ module.exports = [
     text: '兴趣爱好',
     link: '/like/',
     items: [
-      // { text: '学习', link: '/pages/f2a556/' },
-      // { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+      { text: '英语', link: '/pages/4a7964/' },
+      { text: '日语', link: '/pages/c711cb/' },
       // { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
       // { text: '实用技巧', link: '/pages/baaa02/' },
       // { text: '友情链接', link: '/friends/' },
